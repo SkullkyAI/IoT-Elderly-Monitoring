05092a764c5b536773316dcdcc39e54f05affbc9
GabrielJuan349 <81719519+GabrielJuan349@users.noreply.github.com>
2024-11-19 00:01:57 +0100

    Avanzando en el front

--- WaveSafe/src/app/components/navbar/navbar.component.ts
+++ WaveSafe/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   standalone: true,
   imports: [
     CommonModule,
   ],
-  template: `<p>navbar works!</p>`,
+  templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  alertList: Array<number | string> = [1, 2, 3, 4, 5];
+  alert = signal<number|string>(0);
+
+  constructor() {
+    console.log(this.alertList.length);
+    this.alert.set(this.alertList.length);
+  }
+
+  signOut(): void {
+    // sign out logic
+    console.log('Signing out...');
+    // clear local storage or session storage
+    localStorage.removeItem('token');
+  }
+ }
--- WaveSafe/src/app/pages/homePage/homePage.component.ts
+++ WaveSafe/src/app/pages/homePage/homePage.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    NavbarComponent
+],
   templateUrl: './homePage.component.html',
   styleUrl: './homePage.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
